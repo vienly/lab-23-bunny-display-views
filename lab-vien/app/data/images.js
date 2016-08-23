f5848997f54e45a64fdf56ef8934f00e2ebb8021
Gin <vienlyt@gmail.com>
2016-08-22 20:36:04 -0700

    done

--- lab-vien/app/data/images.js
+++ lab-vien/app/data/images.js
@@ -5,28 +5,32 @@ module.exports = [
     id: '1',
     title: 'Pierrot le fou',
     year: 1965,
+    imdb: 'http://www.imdb.com/title/tt0059592/',
     url: 'http://67.media.tumblr.com/717397a7995f9df4e9aff45f4bc607e7/tumblr_nkhn8iuqFI1qghl49o1_540.gif',
-    description: 'You need more than guts to be a good gangster. You need ideas.'
+    quote: 'Why do you look so sad? Because you speak to me in words and I look at you with feelings.'
   },
   {
     id: 2,
     title: 'Lawless',
     year: 2012,
+    imdb: 'http://www.imdb.com/title/tt1212450/',
     url: 'http://67.media.tumblr.com/2ca5e2fb272a8da435019ecf03ef1809/tumblr_o19ni22r811qghl49o1_540.gif',
-    description: 'I have to tell you something.'
+    quote: 'I have to tell you something.'
   },
   {
     id: 3,
     title: 'The Man Who Fell to Earth',
     year: 1976,
+    imdb: 'http://www.imdb.com/title/tt0074851/',
     url: 'http://67.media.tumblr.com/28cfc9ba091d5bd16904f3f594214dbe/tumblr_o0wdpnWDVO1qghl49o1_540.gif',
-    description: 'Well I’m not a scientist. But I know all things begin and end in eternity.'
+    quote: 'Well I’m not a scientist. But I know all things begin and end in eternity.'
   },
   {
     id: 4,
     title: 'Reaper',
-    year: 2015,
+    year: 2014,
+    imdb: 'http://www.imdb.com/title/tt3833134/',
     url: 'http://67.media.tumblr.com/2194445119d74930906e69a713fc2e8c/tumblr_nt1b9wMrXZ1qghl49o1_540.gif',
-    description: 'Wait for me here.'
+    quote: 'Wait for me here.'
   }
 ];
