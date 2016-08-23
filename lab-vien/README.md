4cdf45b5c4a5e5be08b116df3ca5e19c19219ce1
Gin <vienlyt@gmail.com>
2016-08-23 11:37:08 -0700

    done and tested

--- lab-vien/README.md
+++ lab-vien/README.md
@@ -1,1 +1,18 @@
-#401n1 Angular Boilerplate 
+# Cinemagraph gallery
+
+Clone down then run in terminal
+```
+npm i
+```
+To run tests
+```
+karma start
+```
+To lint files
+```
+npm run lint
+```
+To start server
+```
+npm run watch
+```
--- lab-vien/test/controller-test.js
+++ lab-vien/test/controller-test.js
@@ -7,11 +7,23 @@ describe('testing controllers used in routing', function() {
     angular.mock.module('cinemagraphApp');
     angular.mock.inject(($controller) => {
       this.homeController = new $controller('HomeController');
+      this.fullsizeController = new $controller('FullsizeController');
+      this.errorController = new $controller('ErrorController');
+      this.thumbnailController = new $controller('ThumbnailController');
+
     });
   });
 
   it('home controller should create a list of messages', () => {
     expect(this.homeController.images.length > 0).toEqual(true);
     expect(this.homeController.images.length).toEqual(4);
+
+    expect(this.fullsizeController.images.length > 0).toEqual(true);
+    expect(this.fullsizeController.images.length).toEqual(4);
+
+    expect(this.thumbnailController.images.length > 0).toEqual(true);
+    expect(this.thumbnailController.images.length).toEqual(4);
+
+    expect(this.errorController.errorMessage).toEqual('Error trying to access unknown page');
   });
 });
